f2b205a80f6b1f12ae3f80e37c90ca6a6b445bcc
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-01-19 14:39:33 +0600

    Search youtube video app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { query: "" };
+
+  onInputChange = (event) => {
+    this.setState({ query: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.query}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
